54925d6c6d6c03e3a42c2539a99c11c0c7e3bd68
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-03-05 23:58:57 -0500

    Se agrega primera parte de construcción de CSV

--- lib/history/cubit/history_cubit.dart
+++ lib/history/cubit/history_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:cansat_interface/history/history.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
+import 'package:to_csv/to_csv.dart' as exportCSV;
 
 part 'history_state.dart';
 
@@ -13,13 +14,59 @@ class HistoryCubit extends Cubit<HistoryState> {
   Future<void> getHistory() async {
     emit(state.copyWith(historyStatus: HistoryStatus.loading));
     try {
-      final metricsBox =
-          await _collection.openBox<Map<dynamic, dynamic>>('metrics');
+      final metricsBox = await _collection.openBox<Map<dynamic, dynamic>>(
+        'metrics',
+      );
       final metricsValues = await metricsBox.getAllValues();
       final historyList = metricsValues.values.map(History.fromJson).toList();
-      emit(state.copyWith(historyStatus: HistoryStatus.success));
+      emit(
+        state.copyWith(
+          historyStatus: HistoryStatus.success,
+          historyList: historyList,
+        ),
+      );
     } catch (e) {
-      emit(state.copyWith(historyStatus: HistoryStatus.failure));
+      // emit(state.copyWith(historyStatus: HistoryStatus.failure));
     }
+  }
+
+  Future<void> exportHistoryItemToCSV(History historyItem, int index) async {
+    final headerRow = <String>[
+      'Fecha',
+      'Hora',
+      'Humedad',
+      'Presión',
+      'Temperatura',
+      'Giroscopio X',
+      'Giroscopio Y',
+      'Giroscopio Z',
+      'Aceleración X',
+      'Aceleración Y',
+      'Aceleración Z',
+      'Altitud',
+    ];
+
+    final listOfListOfStrings = <List<String>>[];
+    for (var i = 0; i < historyItem.humidity.length; i++) {
+      listOfListOfStrings.add([
+        historyItem.start.toIso8601String().substring(0, 10),
+        historyItem.start.toIso8601String().substring(11, 19),
+        historyItem.humidity[i].toString(),
+        historyItem.pressure[i].toString(),
+        historyItem.temperature[i].toString(),
+        historyItem.gyroscopeX[i].toString(),
+        historyItem.gyroscopeY[i].toString(),
+        historyItem.gyroscopeZ[i].toString(),
+        historyItem.accelerationX[i].toString(),
+        historyItem.accelerationY[i].toString(),
+        historyItem.accelerationZ[i].toString(),
+        historyItem.altitude[i].toString(),
+      ]);
+    }
+
+    await exportCSV.myCSV(
+      headerRow,
+      listOfListOfStrings,
+    );
   }
 }
--- lib/history/view/history_view.dart
+++ lib/history/view/history_view.dart
@@ -1,6 +1,7 @@
 import 'package:cansat_interface/history/history.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 class HistoryView extends StatelessWidget {
   const HistoryView({super.key});
@@ -16,17 +17,133 @@ class HistoryView extends StatelessWidget {
             child: ProgressRing(),
           );
         } else if (state.historyStatus.isSuccess) {
-          return const Center(
-            child: Text('History loaded'),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: state.historyList.length,
+              itemBuilder: (context, index) {
+                return HistoryItemCard(
+                  historyItem: state.historyList[index],
+                  index: index,
+                );
+              },
+            ),
           );
         } else if (state.historyStatus.isFailure) {
           return const Center(
-            child: Text('Failed to load history'),
+            child: Text(
+              'Error al cargar el historial',
+              style: TextStyle(fontFamily: 'Roboto-Medium'),
+            ),
           );
         } else {
           return const SizedBox.shrink();
         }
       },
+    );
+  }
+}
+
+class HistoryItemCard extends StatelessWidget {
+  const HistoryItemCard({
+    required this.historyItem,
+    required this.index,
+    super.key,
+  });
+
+  final History historyItem;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        borderRadius: BorderRadius.circular(10),
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Inicio',
+                          style: TextStyle(
+                            fontFamily: 'Roboto-Medium',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          DateFormat('dd-MM-yyyy HH:mm:ss')
+                              .format(historyItem.start),
+                          style: const TextStyle(fontFamily: 'Roboto-Medium'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Fin',
+                          style: TextStyle(
+                            fontFamily: 'Roboto-Medium',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          DateFormat('dd-MM-yyyy HH:mm:ss')
+                              .format(historyItem.end),
+                          style: const TextStyle(fontFamily: 'Roboto-Medium'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Registros',
+                          style: TextStyle(
+                            fontFamily: 'Roboto-Medium',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          '${historyItem.humidity.length}',
+                          style: const TextStyle(fontFamily: 'Roboto-Medium'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            FilledButton(
+              child: const Text(
+                'Guardar\ncomo CSV',
+                style: TextStyle(fontFamily: 'Roboto-Medium'),
+              ),
+              onPressed: () => context
+                  .read<HistoryCubit>()
+                  .exportHistoryItemToCSV(historyItem, index),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
