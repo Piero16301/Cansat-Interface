268fa9d644e68290e01e821146582810fddfd532
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-03-07 12:40:15 -0500

    Cambio ruta DB Hive

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,18 @@ import 'package:cansat_interface/app/app.dart';
 import 'package:cansat_interface/bootstrap.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   bootstrap(() async {
     WidgetsFlutterBinding.ensureInitialized();
     final preferences = await SharedPreferences.getInstance();
+    final docsDir = await getApplicationDocumentsDirectory();
     final collection = await BoxCollection.open(
       'CANSATTelemetry',
       {'metrics'},
-      path: './',
+      path: '${docsDir.path}/cansat-telemetry/',
     );
     return App(
       preferences: preferences,
